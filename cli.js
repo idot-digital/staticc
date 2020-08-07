4ff0dff5550f90746628b1c5758c00dd259ff76d
Luis Kugel <luis@kugelx.de>
2020-08-07 10:09:33 +0200

    prettifyed code

--- cli.js
+++ cli.js
@@ -1,117 +1,138 @@
 #! /usr/bin/env node
-const {readFileFromDisk, saveFileToDisk, transpile} = require('./lib')
-const express = require('express')
-const morgan = require('morgan')
-const minify = require('html-minifier').minify;
-const glob = require("glob")
+const {
+  readFileFromDisk,
+  saveFileToDisk,
+  transpile,
+  getImportedFiles,
+} = require("./lib");
+const express = require("express");
+const morgan = require("morgan");
+const minify = require("html-minifier").minify;
+const glob = require("glob");
 const args = process.argv.slice(2);
-const fs = require('fs')
+const fs = require("fs");
 
-const help = args.indexOf('--help') >= 0 || args.indexOf('-h') >= 0 || args.indexOf('help') >= 0;
-const version = args.indexOf('version') >= 0 || args.indexOf('v') >= 0 || args.indexOf('-v') >= 0
-const build_dev = args.indexOf('build-dev') >= 0;
-const build_prod = args.indexOf('build') >= 0;
-const serve = args.indexOf('serve') >= 0;
-const init = args.indexOf('init') >= 0;
+const help =
+  args.indexOf("--help") >= 0 ||
+  args.indexOf("-h") >= 0 ||
+  args.indexOf("help") >= 0;
+const version =
+  args.indexOf("version") >= 0 ||
+  args.indexOf("v") >= 0 ||
+  args.indexOf("-v") >= 0;
+const build_dev = args.indexOf("build-dev") >= 0;
+const build_prod = args.indexOf("build") >= 0;
+const serve = args.indexOf("serve") >= 0;
+const init = args.indexOf("init") >= 0;
 
 let error;
 let data;
 try {
-    data = JSON.parse(readFileFromDisk("data.json"))
+  data = JSON.parse(readFileFromDisk("data.json"));
 } catch (e) {
-    error = "Could not open data.json!"
+  error = "Could not open data.json!";
 }
 
-
-if(version){
-	const package_info = require("./package.json")
-	console.log(package_info.version)
-}else if(help){
-	console.log("")
-	console.log("Usage: staticc <command>")
-	console.log("")
-	console.log("where: <command> is one of:")
-	console.log("v\t\t alias for version")
-	console.log("version\t\t shows the version of the staticc-cli")
-	console.log("build\t\t creates a production build of all html files")
-	console.log("build-dev\t creates a development build of all html files")
-	console.log("serve\t\t starts a development webserver")
-	console.log("init\t\t initializes a new staticc project")
-	console.log("")
-	console.log("Visit https://github.com/luiskugel/staticc to learn more about staticc.")
-}else if(build_dev || build_prod){
-	if(error) {console.log(error); return}
-	console.log("")
-	console.log("starting build!")
-	const HTMLfiles = glob.sync("src/**/*.html")
-	copyAllFiles()
-	HTMLfiles.forEach(file => {
-		console.log("Building: " + file)
-		const inputFile = readFileFromDisk(file)
-		let transpiledCode = transpile(inputFile, data)
-		if(build_prod) transpiledCode = minifyHTML(transpiledCode)
-    	saveFileToDisk(changeFilenameFromSrcToDist(file), transpiledCode)
-	});
-    console.log("finished build!")
-}else if(serve){
-    if(error) {console.log(error); return}
-    startServer()
-}else if(init){
-	console.log("")
-	console.log("Initializing staticc project!")
-	console.log("")
-	const example_project = require("./example_project.json")
-	Object.keys(example_project).forEach(filepath => {
-		saveFileToDisk(filepath, example_project[filepath])
-	});
-	console.log("Finished!")
-}else{
-    console.log("Use -h or --help for help!")
+if (version) {
+  const package_info = require("./package.json");
+  console.log(package_info.version);
+} else if (help) {
+  console.log("");
+  console.log("Usage: staticc <command>");
+  console.log("");
+  console.log("where: <command> is one of:");
+  console.log("v\t\t alias for version");
+  console.log("version\t\t shows the version of the staticc-cli");
+  console.log("build\t\t creates a production build of all html files");
+  console.log("build-dev\t creates a development build of all html files");
+  console.log("serve\t\t starts a development webserver");
+  console.log("init\t\t initializes a new staticc project");
+  console.log("");
+  console.log(
+    "Visit https://github.com/luiskugel/staticc to learn more about staticc."
+  );
+} else if (build_dev || build_prod) {
+  if (error) {
+    console.log(error);
+    return;
+  }
+  console.log("");
+  console.log("starting build!");
+  const HTMLfiles = glob.sync("src/**/*.html");
+  HTMLfiles.forEach((file) => {
+    console.log("Building: " + file);
+    const inputFile = readFileFromDisk(file);
+    let transpiledCode = transpile(inputFile, data);
+    if (build_prod) transpiledCode = minifyHTML(transpiledCode);
+    saveFileToDisk(changeFilenameFromSrcToDist(file), transpiledCode);
+  });
+  copyAllFiles(getImportedFiles());
+  console.log("finished build!");
+} else if (serve) {
+  if (error) {
+    console.log(error);
+    return;
+  }
+  startServer();
+} else if (init) {
+  console.log("");
+  console.log("Initializing staticc project!");
+  console.log("");
+  const example_project = require("./example_project.json");
+  Object.keys(example_project).forEach((filepath) => {
+    saveFileToDisk(filepath, example_project[filepath]);
+  });
+  console.log("Finished!");
+} else {
+  console.log("Use -h or --help for help!");
 }
 
-function startServer () {
-	const app = express()
-	app.use(morgan('dev'))
+function startServer() {
+  const app = express();
+  app.use(morgan("dev"));
 
-	app.use((req, res, next)=>{
-		if(req.url == "/"){
-			req.url += "index.html"
-		}
-		const urlParts = req.url.split(".")
-		if(urlParts[urlParts.length-1] == "html"  || req.url == "/"){
-			const inputFile = readFileFromDisk("src/" + req.url)
-			res.write(transpile(inputFile, data))
-			res.end()
-		}else{
-			next()
-		}
-		
-	})
-	app.use(express.static('src')) 
-	app.listen(8888, () => {
-		console.log("Staticc server listening on http://localhost:8888/")
-	  })
+  app.use((req, res, next) => {
+    if (req.url == "/") {
+      req.url += "index.html";
+    }
+    const urlParts = req.url.split(".");
+    if (urlParts[urlParts.length - 1] == "html" || req.url == "/") {
+      const inputFile = readFileFromDisk("src/" + req.url);
+      res.write(transpile(inputFile, data));
+      res.end();
+    } else {
+      next();
+    }
+  });
+  app.use(express.static("src"));
+  app.listen(8888, () => {
+    console.log("Staticc server listening on http://localhost:8888/");
+  });
 }
 
-function minifyHTML(html_String){
-	return minify(html_String, {
-		removeComments: true,
-		collapseWhitespace: true,
-		minifyCSS: true,
-		minifyJS: true
-	});
+function minifyHTML(html_String) {
+  return minify(html_String, {
+    removeComments: true,
+    collapseWhitespace: true,
+    minifyCSS: true,
+    minifyJS: true,
+  });
 }
 
-function copyAllFiles(){
-	const allfiles = glob.sync("src/**/*.*")
-	allfiles.forEach(file => {
-		const newFilepath = changeFilenameFromSrcToDist(file)
-		const folderpath = newFilepath.split("/").splice(0, newFilepath.split("/").length-1).join("/")
-		if(folderpath) fs.mkdirSync(folderpath, { recursive: true });
-		fs.copyFileSync(file, newFilepath)
-	});
+function copyAllFiles(filter) {
+  const allfiles = glob.sync("src/**/*.*");
+  allfiles.forEach((file) => {
+    if (filter.includes(file)) return;
+    const newFilepath = changeFilenameFromSrcToDist(file);
+    const folderpath = newFilepath
+      .split("/")
+      .splice(0, newFilepath.split("/").length - 1)
+      .join("/");
+    if (folderpath) fs.mkdirSync(folderpath, { recursive: true });
+    fs.copyFileSync(file, newFilepath);
+  });
 }
 
-function changeFilenameFromSrcToDist(file){
-	return "dist" + file.substring(3)
+function changeFilenameFromSrcToDist(file) {
+  return "dist" + file.substring(3);
 }
--- lib.js
+++ lib.js
@@ -1,17 +1,19 @@
 const fs = require("fs");
 
+const importedFiles = [];
+
 const transpile = (input_string, data) => {
-	//splits text into normal html code and code snippets 
-	const [plainHTMLSnippets, codeSnippets] = seperateSnippets(input_string)
-	//convertes code snippets to actual value
-	const resolvedSnippets = resolveSnippets(codeSnippets, data)
-	//recombines html with the resolved code snippets
-	let result = resolvedSnippets.reduce((total, currentValue, currentIndex)=>{
-		return total + plainHTMLSnippets[currentIndex] + currentValue
-	}, "")
-	result += plainHTMLSnippets[plainHTMLSnippets.length-1]
-	return result
-}
+  //splits text into normal html code and code snippets
+  const [plainHTMLSnippets, codeSnippets] = seperateSnippets(input_string);
+  //convertes code snippets to actual value
+  const resolvedSnippets = resolveSnippets(codeSnippets, data);
+  //recombines html with the resolved code snippets
+  let result = resolvedSnippets.reduce((total, currentValue, currentIndex) => {
+    return total + plainHTMLSnippets[currentIndex] + currentValue;
+  }, "");
+  result += plainHTMLSnippets[plainHTMLSnippets.length - 1];
+  return result;
+};
 
 const seperateSnippets = (input_string) => {
   //count number of {{ => number of code blocks
@@ -21,17 +23,17 @@ const seperateSnippets = (input_string) => {
   //for every code block, get the plain html and the code block and add it to the lists
   for (let i = 0; i < oc; i++) {
     const [firstPart, middlePart, lastPart] = cutString(input_string);
-		plainHTMLSnippets.push(firstPart);
-		codeSnippets.push(middlePart);
-		input_string = lastPart;
-	}
-	plainHTMLSnippets.push(input_string);
-	return [plainHTMLSnippets, codeSnippets]
-}
+    plainHTMLSnippets.push(firstPart);
+    codeSnippets.push(middlePart);
+    input_string = lastPart;
+  }
+  plainHTMLSnippets.push(input_string);
+  return [plainHTMLSnippets, codeSnippets];
+};
 
 const occurrences = (string, subString) => {
   return string.split(subString).length - 1;
-}
+};
 
 const cutString = (input_string) => {
   const openingIndex = input_string.indexOf("{{");
@@ -40,128 +42,141 @@ const cutString = (input_string) => {
   const middlePart = input_string.slice(openingIndex + 2, cloringIndex);
   const lastPart = input_string.slice(cloringIndex + 2);
   return [firstPart, middlePart, lastPart];
-}
+};
 
 const resolveSnippets = (jsSnippets_array, data) => {
-	return jsSnippets_array.map(snippet => {
-		const js = snippet.indexOf("#")
-		const prefab = snippet.indexOf("!")
-		const cmd = snippet.indexOf("?")
-		
-		if(js != -1){
-			return resolveJsSnippet(snippet, data)
-		}else if(prefab != -1){
-			return resolvePrefabSnippet(snippet, data)
-		}else if(cmd != -1){
-			return resolveCmdSnippet(snippet)
-		}else{
-			return resolveDataSnippet(snippet, data)
-		}
-	})
-}
+  return jsSnippets_array.map((snippet) => {
+    const js = snippet.indexOf("#");
+    const prefab = snippet.indexOf("!");
+    const cmd = snippet.indexOf("?");
+
+    if (js != -1) {
+      return resolveJsSnippet(snippet, data);
+    } else if (prefab != -1) {
+      return resolvePrefabSnippet(snippet, data);
+    } else if (cmd != -1) {
+      return resolveCmdSnippet(snippet);
+    } else {
+      return resolveDataSnippet(snippet, data);
+    }
+  });
+};
 
 const resolveJsSnippet = (snippet_string, data) => {
   //remove spaces and the #
-  snippet_string = snippet_string.trim().replace('#','');
+  snippet_string = snippet_string.trim().replace("#", "");
   //run the js code and convert string array to array
-	const evaluated_snippet = eval(snippet_string)
-	return noramlizeJsReturns(evaluated_snippet)
-}
+  const evaluated_snippet = eval(snippet_string);
+  return noramlizeJsReturns(evaluated_snippet);
+};
 
 const resolvePrefabSnippet = (snippet_string, data) => {
   //remove spaces and first !
-  snippet_string = snippet_string.trim().replace('!','');
+  snippet_string = snippet_string.trim().replace("!", "");
   //check if its a js or html prefab
-	if(snippet_string.indexOf("!") != -1){
+  if (snippet_string.indexOf("!") != -1) {
     //=> JS snippet
     //remove the second ! and seperate the path of the snippet from the args
-	snippet_string_parts = snippet_string.replace('!','').split(" ");
-    snippet_path = snippet_string_parts.shift()
+    snippet_string_parts = snippet_string.replace("!", "").split(" ");
+    snippet_path = snippet_string_parts.shift();
     //read in the file
-    const jsFile = readFileFromDisk("prefabs/" +  snippet_path + ".js");
-    //parse the js code 
-    const evalResult = eval(jsFile)
+    const jsFile = readFileFromDisk("prefabs/" + snippet_path + ".js");
+    //parse the js code
+    const evalResult = eval(jsFile);
     //run the render function with the provided args. Then convert string array to array if neccesary
-		return noramlizeJsReturns(render(...decodePrefabArgs(snippet_string_parts, data)))
-	}else{
+    return noramlizeJsReturns(
+      render(...decodePrefabArgs(snippet_string_parts, data))
+    );
+  } else {
     //=> HTML snippet
     //read in html file from disk and return it
-		return readFileFromDisk("prefabs/" +  snippet_string + ".html");
-	}
-}
+    importedFiles.push("prefabs/" + snippet_string + ".html");
+    return readFileFromDisk("prefabs/" + snippet_string + ".html");
+  }
+};
 
 const resolveCmdSnippet = (snippet_string) => {
-	//remove spaces and ?
-	snippet_string_parts = snippet_string.trim().replace('?','').split(" ");;
-	snippet_cmd = snippet_string_parts.shift()
-	switch(snippet_cmd) {
-		case "svg":
-		  return importSvg(...snippet_string_parts)
-		case "css":
-		  return importCss(...snippet_string_parts)
-		default:
-		  return ""
-	  } 
-}
+  //remove spaces and ?
+  snippet_string_parts = snippet_string.trim().replace("?", "").split(" ");
+  snippet_cmd = snippet_string_parts.shift();
+  switch (snippet_cmd) {
+    case "svg":
+      return importSvg(...snippet_string_parts);
+    case "css":
+      return importCss(...snippet_string_parts);
+    default:
+      return "";
+  }
+};
 
 const resolveDataSnippet = (snippet_string, data) => {
-  let value = data
-  const snippetParts = snippet_string.replace(/\s/g,'').split(".")
+  let value = data;
+  const snippetParts = snippet_string.replace(/\s/g, "").split(".");
   for (let i = 0; i < snippetParts.length; i++) {
-    value = value[snippetParts]
+    value = value[snippetParts];
   }
-	return value
-}
+  return value;
+};
 
 const noramlizeJsReturns = (evaluated_snippet) => {
   //check if the evaluated snippet is a string which can be returned or if its an array which needs to be reduced
-	if(evaluated_snippet.constructor == String){
-		return evaluated_snippet
-	}else if(evaluated_snippet.constructor == Array){
-		return evaluated_snippet.reduce((total, current)=>{
-			return total + current
-		})
-	}else{
-		throw new Error("only strings or array of strings are allowed")
-	}
-}
+  if (evaluated_snippet.constructor == String) {
+    return evaluated_snippet;
+  } else if (evaluated_snippet.constructor == Array) {
+    return evaluated_snippet.reduce((total, current) => {
+      return total + current;
+    });
+  } else {
+    throw new Error("only strings or array of strings are allowed");
+  }
+};
 
 const decodePrefabArgs = (args, data) => {
-	args = args.map(arg=>{
-		if(arg == "") return null;
-		if(arg.charAt(0) == '"'){
-			arg = arg.substring(1, arg.length-1)
-			return arg
-		}else{
-			return data[arg]
-		}
-	})
-	return args
-}
+  args = args.map((arg) => {
+    if (arg == "") return null;
+    if (arg.charAt(0) == '"') {
+      arg = arg.substring(1, arg.length - 1);
+      return arg;
+    } else {
+      return data[arg];
+    }
+  });
+  return args;
+};
 
 const importSvg = (svgPath) => {
-	return readFileFromDisk("src/" + svgPath)
-}
+  importedFiles.push("src/" + svgPath);
+  return readFileFromDisk("src/" + svgPath);
+};
 
-const importCss = (svgPath) => {
-	const styleSheet = readFileFromDisk("src/" + svgPath)
-	return `<style>${styleSheet}</style>`
-}
+const importCss = (cssPath) => {
+  importedFiles.push("src/" + cssPath);
+  const styleSheet = readFileFromDisk("src/" + cssPath);
+  return `<style>${styleSheet}</style>`;
+};
 
 const readFileFromDisk = (filepath) => {
   //read file from disk
-	return fs.readFileSync(filepath, {
-		encoding: "utf8"
-	});
-}
+  return fs.readFileSync(filepath, {
+    encoding: "utf8",
+  });
+};
 
 const saveFileToDisk = (filepath, content) => {
   //save file to disk (+ create folders if neccesary)
-	const folderpath = filepath.split("/").splice(0, filepath.split("/").length-1).join("/")
-	if(folderpath) fs.mkdirSync(folderpath, { recursive: true });
-	fs.writeFileSync(filepath, content);
-}
+  const folderpath = filepath
+    .split("/")
+    .splice(0, filepath.split("/").length - 1)
+    .join("/");
+  if (folderpath) fs.mkdirSync(folderpath, { recursive: true });
+  fs.writeFileSync(filepath, content);
+};
+
+const getImportedFiles = () => {
+  return importedFiles;
+};
 
 exports.saveFileToDisk = saveFileToDisk;
 exports.readFileFromDisk = readFileFromDisk;
 exports.transpile = transpile;
+exports.getImportedFiles = getImportedFiles;
