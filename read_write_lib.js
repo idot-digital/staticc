48ae57e4f5bfcbf1c592173765021eca1840bd10
Luis Kugel <luis@kugelx.de>
2020-10-22 09:49:00 +0200

    fixed importing errors

--- read_write_lib.js
+++ read_write_lib.js
@@ -1,20 +1,23 @@
+const fs = require("fs");
+const pathLib = require("path");
+
 const readFileFromDisk = (filepath) => {
     //read file from disk
     return fs.readFileSync(filepath, {
-      encoding: "utf8",
+        encoding: "utf8",
     });
-  };
+};
   
-  const saveFileToDisk = (filepath, content) => {
+const saveFileToDisk = (filepath, content) => {
     //save file to disk (+ create folders if neccesary)
     const folderpath = pathLib.join(...(filepath.split("/").splice(0, filepath.split("/").length - 1)));
     if (folderpath) fs.mkdirSync(folderpath, { recursive: true });
     fs.writeFileSync(filepath, content);
-  };
+};
   
-  const fileExists = (filepath) => {
+const fileExists = (filepath) => {
     return fs.existsSync(filepath)
-  }
+}
 
 exports.readFileFromDisk = readFileFromDisk;
 exports.saveFileToDisk = saveFileToDisk;
